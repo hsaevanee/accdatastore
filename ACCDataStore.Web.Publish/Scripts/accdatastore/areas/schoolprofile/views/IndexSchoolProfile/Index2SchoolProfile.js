ef2eabc98193692e07e73af2229f63a6dbdbe9bb
hsaevanee <hataichanok.saevanee@gmail.com>
2015-06-19 14:17:16 +0100

    team ready prototype

---
+++ ACCDataStore.Web.Publish/Scripts/accdatastore/areas/schoolprofile/views/IndexSchoolProfile/Index2SchoolProfile.js
@@ -0,0 +1,35 @@
+﻿
+$(document).ready(function () {
+
+
+    $("#selectedschoolname").change(function () {
+        //alert("selectedschoolname");
+        //window.location = sContextPath + 'SchoolProfile/IndexSchoolProfile/Index/'+$(this).val();
+        if (validateDropdownlist()==true) {
+            document.forms[0].submit();
+        }        
+    });
+
+    $("#selectedschoolname2").change(function () {
+        //alert("selectedschoolname2");
+        //window.location = sContextPath + 'SchoolProfile/IndexSchoolProfile/Index/'+$(this).val();
+        if (validateDropdownlist() == true) {
+            document.forms[0].submit();
+        }
+    });
+
+
+});
+
+function validateDropdownlist() {
+    var value1 = $('#selectedschoolname :selected').text();
+    var value2 = $('#selectedschoolname2 :selected').text();
+
+    if (value1 == "---Please Select School---" && value2 == "---Please Select School---") {
+        alert('Please select School');
+        return false;
+    } else {
+        return true;
+    }
+
+}
---
+++ ACCDataStore.Web.Publish2/Scripts/accdatastore/areas/schoolprofile/views/EthnicBackground/EthnicBackground.js
@@ -0,0 +1,214 @@
+﻿var mSchCriteriaParams, mdataParams;
+var dataNationality;
+
+$(function () {
+    InitSpinner();
+});
+
+$(document).ready(function () {
+
+    $("input[name='ethnicity']").click(function () {
+        $('input[name="CheckEthnicityAll"]').prop("checked", false);
+    });
+
+    $("input[name='CheckEthnicityAll']").change(function () {
+        if (this.checked) {
+            //alert('ChecknationalityAll check');
+            $('input[name="ethnicity"]').prop("checked", true);
+        } else {
+            $('input[name="ethnicity"]').prop("checked", false);
+        }
+    });
+
+    $("input[name='CheckDataitem']").click(function () {
+        $('input[name="CheckDataitemAll"]').prop("checked", false);
+    });
+
+    $("input[name='CheckDataitemAll']").change(function () {
+        if (this.checked) {
+            //alert('ChecknationalityAll check');
+            $('input[name="CheckDataitem"]').prop("checked", true);
+        } else {
+            $('input[name="CheckDataitem"]').prop("checked", false);
+        }
+    });
+});
+
+function validateCheckBoxs() {
+    // get all checked checkbox
+    var arrCheckboxCheckedEthnic = [];
+    $('input[name="ethnicity"]:checked').each(function () {
+        arrCheckboxCheckedEthnic.push($(this).val());
+    });
+
+    var sSchoolNameText = $('#selectSchoolname option:selected').text();
+
+    mSchCriteriaParams = {
+        selectedschoolname: sSchoolNameText,
+        ListConditionEthnicbg: arrCheckboxCheckedEthnic,        
+    };
+
+    if (arrCheckboxCheckedEthnic.length == 0) {
+        alert('Please select EthnicBackground');
+        return false;    
+    } else {
+        return true;
+    }
+
+}
+
+
+function myFunctionBar() {
+    var arrCheckboxCheckedCheckDataitem = [];
+    $('input[name="CheckDataitem"]:checked').each(function () {
+        arrCheckboxCheckedCheckDataitem.push($(this).val());
+    });
+
+    if (arrCheckboxCheckedCheckDataitem.length == 0) {
+        alert("Please select data to create graph");
+    } else {
+        $.ajax({
+            type: 'POST',
+            url: sContextPath + 'SchoolProfile/EthnicBackground/GetChartDataEthnic',
+            data: JSON.stringify(arrCheckboxCheckedCheckDataitem),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (data) {
+                drawChartBar(data);
+            },
+            error: function (xhr, err) {
+                if (xhr.readyState != 0 && xhr.status != 0) {
+                    alert('readyState: ' + xhr.readyState + '\nstatus: ' + xhr.status);
+                    alert('responseText: ' + xhr.responseText);
+                }
+            }
+        });
+    } 
+}
+
+function drawChartBar(data) {
+    $('#divChartContainer')
+            .highcharts(
+                    {
+                        chart: {
+                            type: 'bar'
+                        },
+                        title: {
+                            text: 'Ethnic Background - Primary Schools (%pupils)'
+                        },
+                        subtitle: {
+                            text: ''
+                        },
+                        xAxis: {
+                            //categories: [ '0%', '5%', '10%', '15%','20%','25%','30%'],
+                            categories: data.ChartCategories,
+                            title: {
+                                text: 'Deciles'
+                            }
+                        },
+                        yAxis: {
+                            min: 0,
+                            title: {
+                                text: '% Pupils in Each Decile (Census 2013)'
+                            }
+                        },
+                        tooltip: {
+                            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                            pointFormat: '<tr><td nowrap style="color:{series.color};padding:0">{series.name}: </td>'
+                                    + '<td style="padding:0"><b>{point.y:.0f}</b></td></tr>',
+                            footerFormat: '</table>',
+                            shared: true,
+                            useHTML: true
+                        },
+                        plotOptions: {
+                            column: {
+                                pointPadding: 0.2,
+                                borderWidth: 0
+                            }
+                        },
+                        series: data.ChartSeries,
+                        credits: {
+                            enabled: false
+                        }
+                    });
+}
+
+function myFunctionColumn() {
+    //alert("myFunctionColumn");
+    var arrCheckboxCheckedCheckDataitem = [];
+    $('input[name="CheckDataitem"]:checked').each(function () {
+        arrCheckboxCheckedCheckDataitem.push($(this).val());
+    });
+
+    if (arrCheckboxCheckedCheckDataitem.length == 0) {
+        alert("Please select data to create graph");
+    } else {
+
+        $.ajax({
+            type: 'POST',
+            url: sContextPath + 'SchoolProfile/EthnicBackground/GetChartDataEthnic',
+            data: JSON.stringify(arrCheckboxCheckedCheckDataitem),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (data) {
+                drawChartColumn(data);
+            },
+            error: function (xhr, err) {
+                if (xhr.readyState != 0 && xhr.status != 0) {
+                    alert('readyState: ' + xhr.readyState + '\nstatus: ' + xhr.status);
+                    alert('responseText: ' + xhr.responseText);
+                }
+            }
+        });
+    }
+
+}
+
+function drawChartColumn(data) {
+    $('#divChartContainer')
+            .highcharts(
+                    {
+                        chart: {
+                            type: 'column'
+                        },
+                        title: {
+                            text: 'Ethnic Background - Primary Schools (%pupils)'
+                        },
+                        subtitle: {
+                            text: ''
+                        },
+                        xAxis: {
+                            //categories: [ '0%', '5%', '10%', '15%','20%','25%','30%'],
+                            categories: data.ChartCategories,
+                            title: {
+                                text: 'Deciles'
+                            }
+                        },
+                        yAxis: {
+                            min: 0,
+                            title: {
+                                text: '% Pupils in Each Decile (Census 2013)'
+                            }
+                        },
+                        tooltip: {
+                            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                            pointFormat: '<tr><td nowrap style="color:{series.color};padding:0">{series.name}: </td>'
+                                    + '<td style="padding:0"><b>{point.y:.0f}</b></td></tr>',
+                            footerFormat: '</table>',
+                            shared: true,
+                            useHTML: true
+                        },
+                        plotOptions: {
+                            column: {
+                                pointPadding: 0.2,
+                                borderWidth: 0
+                            }
+                        },
+                        series: data.ChartSeries,
+                        credits: {
+                            enabled: false
+                        }
+                    });
+}
+
+
