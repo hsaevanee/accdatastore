dd2216d7bec49f665352db00ab531ba1e61fec8e
hsaevanee <hataichanok.saevanee@gmail.com>
2015-08-24 15:51:36 +0100

    initial InsightProfile

---
+++ ACCDataStore.Web/Areas/InsightProfile/InsightProfileAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ACCDataStore.Web.Areas.InsightProfile
+{
+    public class InsightProfileAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "InsightProfile";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "InsightProfile_default",
+                "InsightProfile/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
