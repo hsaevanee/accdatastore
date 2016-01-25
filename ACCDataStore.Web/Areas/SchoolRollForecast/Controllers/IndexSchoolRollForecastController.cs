eb817152c86f48ed250700df796a85ea74e4b57a
DonDons <d.connelly.14@aberdeen.ac.uk>
2016-01-25 05:41:43 +0000

    school roll forecast

---
+++ ACCDataStore.Web/Areas/SchoolRollForecast/Controllers/IndexSchoolRollForecastController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ACCDataStore.Web.Areas.SchoolRollForecast.Controllers
+{
+    public class IndexSchoolRollForecastController : Controller
+    {
+        // GET: SchoolRollForecast/IndexSchoolRollForecast
+        public ActionResult Index()
+        {
+            return View("Home");
+
+
+        }
+    }
+}
+//put excel code in here
---
+++ ACCDataStore.Web/Areas/SchoolRollForecast/SchoolRollForecastAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ACCDataStore.Web.Areas.SchoolRollForecast
+{
+    public class SchoolRollForecastAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SchoolRollForecast";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SchoolRollForecast_default",
+                "SchoolRollForecast/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
