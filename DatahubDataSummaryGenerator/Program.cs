06dc26f5ac693308b4408f6f4cd0f2fece394f07
hsaevanee <hataichanok.saevanee@gmail.com>
2016-08-24 12:02:01 +0100

    Created a separed program to generate summary data from datahub student data (work in progress); Added entities and mappings to work with scotland map data tables

---
+++ DatahubDataSummaryGenerator/Program.cs
@@ -0,0 +1,45 @@
+﻿using ACCDataStore.Entity.DatahubProfile;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatahubDataSummaryGenerator
+{
+    class Program
+    {
+        private static ISessionFactory CreateSessionFactory()
+        {
+            //var connectionString = "server=localhost;user id=root;password=toor;persist security info=True;database=simd";
+            return Fluently.Configure()
+                .Database(
+                    MySQLConfiguration.Standard.ConnectionString(
+                        c => c.FromConnectionStringWithKey("ConnectionString")
+                    )
+                )
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<ACCDataStore.Entity.BaseEntity>())
+                .BuildSessionFactory();
+        }
+        static void Main(string[] args)
+        {
+            var sessionFactory = CreateSessionFactory();
+            using (var session = sessionFactory.OpenSession())
+            {
+                using (session.BeginTransaction())
+                {
+                    var str = session.Get<DataZoneObj>("S01000001").Reference_Parent;
+                    // insert magic code
+                    IList datahubStudentData = session.CreateCriteria<DatahubDataObj>().List();
+                    Console.WriteLine(str);
+                }
+            }
+            Console.WriteLine("rdy");
+            Console.ReadKey();
+        }
+    }
+}
