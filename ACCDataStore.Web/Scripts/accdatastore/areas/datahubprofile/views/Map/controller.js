fe6503927fbeba8148052754c7a37f1da4d8ac23
hsaevanee <hataichanok.saevanee@gmail.com>
2017-11-29 17:02:16 +0000

    DataHub heatmap added

--- ACCDataStore.Web/Scripts/accdatastore/areas/datahubprofile/views/Map/controller.js
+++ ACCDataStore.Web/Scripts/accdatastore/areas/datahubprofile/views/Map/controller.js
@@ -1,4 +1,4 @@
-﻿angular.module('root.controllers', ['ngSanitize', 'ui.select', 'highcharts-ng', 'datatables', 'ngMap'])
+﻿angular.module('root.controllers', ['ngSanitize', 'ui.select', 'highcharts-ng', 'datatables', 'ngMap', 'rzModule'])
 
 .controller('rootCtrl', function ($scope, $rootScope) {
 
@@ -8,15 +8,22 @@
     var map;
     var mapBounds;
 
+    var dataMin = Number.MAX_VALUE, dataMax = -Number.MAX_VALUE;
+
     var infowindow = new google.maps.InfoWindow();
-    
+
     $scope.mMap = {
     };
-    
+
+    $scope.mMap.bHeatMapShowContent = true;
+
     mapService.getCondition().then(function (response) {
-        $scope.mMap.bShowContent = false;
         $scope.mMap = response.data;
+        $scope.mMap.bShowContent = false;
+        $scope.mMap.bHeatMapShowContent = true;
     }, function (response) {
+        $scope.mMap.bShowContent = false;
+        $scope.mMap.bHeatMapShowContent = true;
     });
 
     google.maps.event.addDomListener(window, 'load', initMap);
@@ -26,23 +33,9 @@
             zoom: 11,
             center: new google.maps.LatLng(57.151810, -2.094451),
             mapTypeId: google.maps.MapTypeId.TERRAIN
-            //mapTypeControl: true,
-            //mapTypeControlOptions: {
-            //    style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-            //    position: google.maps.ControlPosition.RIGHT_BOTTOM
-            //},
-            //zoomControl: true,
-            //zoomControlOptions: {
-            //    position: google.maps.ControlPosition.RIGHT_BOTTOM
-            //},
-            //scaleControl: true,
-            //streetViewControl: true,
-            //streetViewControlOptions: {
-            //    position: google.maps.ControlPosition.RIGHT_BOTTOM
-            //}
         });
         //initial layer set to Neighbourhoods
-        createLayer($scope.selectedLayer);
+        createLayer($scope.mMap.selectedLayer);
     }
 
     $scope.updateDataset = function () {
@@ -50,20 +43,226 @@
         $scope.mMap.selectedLayer = $scope.mMap.selectedLayer;
         $scope.mMap.selectedDataset = $scope.mMap.selectedDataset;
         $scope.mMap.bShowContent = false;
+
+        mapService.LoadHeatMapdata($scope.mMap.selectedDataset.Code).then(function (response) {
+            $scope.mMap.heatmapdata = response.data;
+            map.data.setStyle(styleFeature);
+        }, function (response) {
+            $rootScope.bShowLoading = false;
+        });
+    }
+
+    $scope.updateCatagory = function () {
+        //console.log($scope.dataset_item.code, $scope.dataset_item.name)
+        $scope.mMap.selectedLayer = $scope.mMap.selectedLayer;
+        $scope.mMap.selectedDataset = $scope.mMap.selectedDataset;
+        $scope.mMap.bShowContent = false;
+        //update data for highlight 
+        map.data.setStyle(styleFeature);
     }
 
     $scope.updateLayer = function () {
 
         $scope.mMap.selectedLayer = $scope.mMap.selectedLayer;
         $scope.mMap.selectedDataset = $scope.mMap.selectedDataset;
+        $scope.mMap.selectedDestination = $scope.mMap.selectedDestination;
         $scope.mMap.bShowContent = false;
         //console.log($scope.layer_item.code, $scope.layer_item.name)
         if ($scope.mMap.selectedLayer.Code == 'S01') {
-            createLayer($scope.selectedLayer);
+            $scope.mMap.bHeatMapShowContent = true;
+            createLayer($scope.mMap.selectedLayer);
         } else {
-            createMarker($scope.selectedLayer);
+            $scope.mMap.bHeatMapShowContent = false;
+            createMarker($scope.mMap.selectedLayer);
+        } 
+
+    }
+
+    $scope.otherData = {
+        start: 0,
+        change: 0,
+        end: 0
+    }
+
+    $scope.slider = {
+        minValue: 70,
+        maxValue: 85,
+        options: {
+            floor: 0,
+            ceil: 100,
+            vertical: false,
+            showOuterSelectionBars: false,
+            onEnd: function() {
+                console.info( $scope.slider.minValue + ' ' + $scope.slider.maxValue)
+                map.data.setStyle(styleFeature);
+            }
+        }
+    };
+
+
+    var styleFeature = function (feature) {
+
+        var data = $scope.mMap.heatmapdata;
+
+        var datacatagory = $scope.mMap.selecteddatacatagory;
+
+        let obj = data.find(x => x.seedcode === feature.getProperty('REFNO'));
+
+        if(!obj){
+            alert('notfound');
         }
 
+        var temp = obj.listdata.find(x => x.Code === datacatagory.Code);
+
+        if(temp.Percent < $scope.slider.minValue){
+            if(datacatagory.Code === 'Positive Destination'){
+                color = '#ff0000'//green
+            }else{
+                color = '#1fc600'//green    
+            }
+        
+        }else if(temp.Percent > $scope.slider.maxValue){
+            if(datacatagory.Code === 'Positive Destination'){
+                color = '#1fc600'//green
+            }else{
+                color = '#ff0000'//green    
+            }
+        }else{
+            color = '#fcf823'
+        }
+
+        var outlineWeight = 0.5, zIndex = 1, opacity = 0.5;
+
+            if (feature.getProperty('isColorful')) {
+                color = shadeColor(color,-20);
+            }
+
+
+        return {
+            zIndex: zIndex,
+            fillColor: color,
+            strokeColor: color,
+            strokeWeight: 1,
+            opacity: opacity
+
+        };
+    }
+
+    var createLayer = function (layertype) {
+        //get json data from database via controller 
+        //render on google map
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 11,
+            center: new google.maps.LatLng(57.151810, -2.094451)
+        });
+
+        map.data.addGeoJson(Neighbourhoodsjsondata);
+
+        map.data.setStyle(styleFeature);
+
+        // When the user clicks, set 'isColorful', changing the color of the letters.
+        map.data.addListener('click', function (event) {
+            event.feature.setProperty('isColorful', true);
+
+            mapService.getData($scope.mMap.selectedLayer.Code, $scope.mMap.selecteddatacatagory.Code, event.feature.getProperty('REFNO'), $scope.mMap.selectedDataset.Code).then(function (response) {
+                $rootScope.bShowLoading = false;
+                $scope.mMap = response.data;
+                $scope.mMap.bShowContent = true;
+                $scope.mMap.bHeatMapShowContent = true;
+            }, function (response) {
+                $rootScope.bShowLoading = false;
+            });
+        });
+
+        // When the user hovers, tempt them to click by outlining the letters.
+        // Call revertStyle() to remove all overrides. This will use the style rules
+        // defined in the function passed to setStyle()
+        map.data.addListener('mouseover', function (event) {
+            map.data.revertStyle();
+            map.data.overrideStyle(event.feature, { strokeWeight: 3.5, zIndex: 3});
+            infowindow.setContent(event.feature.getProperty('name'));
+            infowindow.setPosition(event.latLng);
+            //infowindow.setOptions({ pixelOffset: new google.maps.Size(0, -34) });
+            infowindow.open(map);
+        });
+
+        map.data.addListener('mouseout', function (event) {
+            map.data.revertStyle();
+        });
+
+    }
+
+    var createMarker = function (layertype) {
+        //get json data from database via controller 
+        //render on google map
+
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 11,
+            center: new google.maps.LatLng(57.151810, -2.094451)
+        });
+
+        for (var i = 0; i < InsightSchoollocationsjsondata.features.length; i++) {
+            var coords = InsightSchoollocationsjsondata.features[i].geometry.coordinates;
+            var title = InsightSchoollocationsjsondata.features[i].properties.name;
+            var latLng = new google.maps.LatLng(coords[1], coords[0]);
+            var marker = new google.maps.Marker({
+                position: latLng,
+                map: map,
+                title: title
+            });
+
+            (function (i, marker) {
+                google.maps.event.addListener(marker, 'click', function () {
+                    infowindow.setContent('<h2>' + marker.title + '</h2>');
+                    infowindow.open($scope.map, marker);
+
+                    var pos = map.getZoom();
+                    map.setZoom(17);
+                    map.setCenter(marker.getPosition());
+                    window.setTimeout(function () { map.setZoom(pos); }, 3000);
+
+                    var seedcode = InsightSchoollocationsjsondata.features[i].properties.SCHOCODE;
+
+                    mapService.getData($scope.mMap.selectedLayer.Code, $scope.mMap.selecteddatacatagory.Code, seedcode, $scope.mMap.selectedDataset.Code).then(function (response) {
+                        $rootScope.bShowLoading = false;
+                        $scope.mMap = response.data;
+                        $scope.mMap.bShowContent = true;
+                    }, function (response) {
+                        $rootScope.bShowLoading = false;
+                    });
+
+                });
+
+                google.maps.event.addListener(marker, 'mouseover', function () {
+
+                    infowindow.setContent(marker.title);
+                    infowindow.open($scope.map, marker);
+                });
+            })(i, marker);
+        }
+
+    }
+
+    function shadeColor(color, percent) {
+
+        var R = parseInt(color.substring(1,3),16);
+        var G = parseInt(color.substring(3,5),16);
+        var B = parseInt(color.substring(5,7),16);
+
+        R = parseInt(R * (100 + percent) / 100);
+        G = parseInt(G * (100 + percent) / 100);
+        B = parseInt(B * (100 + percent) / 100);
+
+        R = (R<255)?R:255;  
+        G = (G<255)?G:255;  
+        B = (B<255)?B:255;  
+
+        var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
+        var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
+        var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+
+        return "#"+RR+GG+BB;
     }
 
     $scope.GetPositiveChart = function () {
@@ -98,113 +297,7 @@
         $scope.mMap.ChartData.ChartTimelineDestinations.series[4].visible = true;
         $scope.mMap.ChartData.ChartTimelineDestinations.series[5].visible = true;
     };
-
-    var createLayer = function (layertype) {
-        //get json data from database via controller 
-        //render on google map
-
-        map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 11,
-            center: new google.maps.LatLng(57.151810, -2.094451)
-        });
-
-        map.data.addGeoJson(Neighbourhoodsjsondata);
-
-        map.data.setStyle(function (feature) {
-            var color = '#0E94FF';
-            if (feature.getProperty('isColorful')) {
-                color = '#006bc1';
-            }
-            return /** @type {google.maps.Data.StyleOptions} */({
-                fillColor: color,
-                strokeColor: color,
-                strokeWeight: 2
-            });
-        });
-
-        // When the user clicks, set 'isColorful', changing the color of the letters.
-        map.data.addListener('click', function (event) {
-            event.feature.setProperty('isColorful', true);
-
-            mapService.getData($scope.mMap.selectedLayer.Code,event.feature.getProperty('REFNO'), $scope.mMap.selectedDataset.Code).then(function (response) {
-                $rootScope.bShowLoading = false;
-                $scope.mMap = response.data;
-                $scope.mMap.bShowContent = true;
-            }, function (response) {
-                $rootScope.bShowLoading = false;
-            });
-        });
-
-        // When the user hovers, tempt them to click by outlining the letters.
-        // Call revertStyle() to remove all overrides. This will use the style rules
-        // defined in the function passed to setStyle()
-        map.data.addListener('mouseover', function (event) {
-            map.data.revertStyle();
-            map.data.overrideStyle(event.feature, { strokeWeight: 7 });
-            infowindow.setContent(event.feature.getProperty('name'));
-            infowindow.setPosition(event.latLng);
-            //infowindow.setOptions({ pixelOffset: new google.maps.Size(0, -34) });
-            infowindow.open(map);
-        });
-
-        map.data.addListener('mouseout', function (event) {
-            map.data.revertStyle();
-        });
-
-
- 
-    }
-
-    var createMarker = function (layertype) {
-        //get json data from database via controller 
-        //render on google map
-
-        map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 11,
-            center: new google.maps.LatLng(57.151810, -2.094451)
-        });
-
-        for (var i = 0; i < InsightSchoollocationsjsondata.features.length; i++) {
-            var coords = InsightSchoollocationsjsondata.features[i].geometry.coordinates;
-            var title = InsightSchoollocationsjsondata.features[i].properties.name;
-            var latLng = new google.maps.LatLng(coords[1], coords[0]);
-            var marker = new google.maps.Marker({
-                position: latLng,
-                map: map,
-                title: title
-            });
-
-            (function (i, marker) {
-                google.maps.event.addListener(marker, 'click', function () {
-                    infowindow.setContent('<h2>' + marker.title + '</h2>');
-                    infowindow.open($scope.map, marker);
-
-                    var pos = map.getZoom();
-                    map.setZoom(17);
-                    map.setCenter(marker.getPosition());
-                    window.setTimeout(function () { map.setZoom(pos); }, 3000);
-
-                    var seedcode = InsightSchoollocationsjsondata.features[i].properties.SCHOCODE;
-
-                    mapService.getData($scope.mMap.selectedLayer.Code, seedcode, $scope.mMap.selectedDataset.Code).then(function (response) {
-                        $rootScope.bShowLoading = false;
-                        $scope.mMap = response.data;
-                        $scope.mMap.bShowContent = true;
-                    }, function (response) {
-                        $rootScope.bShowLoading = false;
-                    });
-
-                });
-
-                google.maps.event.addListener(marker, 'mouseover', function () {
-
-                    infowindow.setContent(marker.title);
-                    infowindow.open($scope.map, marker);
-                });
-            })(i, marker);
-        }
-
-    }
-   
 });
+
+
  
--- ACCDataStore.Web/Scripts/accdatastore/areas/datahubprofile/views/Map/service.js
+++ ACCDataStore.Web/Scripts/accdatastore/areas/datahubprofile/views/Map/service.js
@@ -6,11 +6,14 @@ angular.module('root.services', [])
         getGeoJSON: function (layertype) {
             return $http.get(sContextPath + "DatahubProfile/Map/GetGeoJson", { params: { "layertype": layertype } });
         },
-        getData: function (layertype,RefNO, dataset) {
-            return $http.get(sContextPath + "DatahubProfile/Map/GetData", { params: { "layertype": layertype, "seedcode": RefNO, "dataset": dataset } });
+        getData: function (layertype, datacatagory, RefNO, dataset) {
+            return $http.get(sContextPath + "DatahubProfile/Map/GetData", { params: { "layertype": layertype, "datacatagory": datacatagory, "seedcode": RefNO, "dataset": dataset } });
         },
         getCondition: function () {
             return $http.get(sContextPath + "DatahubProfile/Map/GetCondition");
-    }
+        },
+        LoadHeatMapdata: function (dataset) {
+            return $http.get(sContextPath + "DatahubProfile/Map/LoadHeatMapdata", { params: { "dataset": dataset } });
+        }
     };
 });
